dce3e3e8477c614f96ea8ad4eaac5e306918f206
martihug <hugo.martin2@etu.univ-grenoble-alpes.fr>
2022-11-18 12:26:30 +0100

    chore: init structure

---
+++ mobileApp/Save-Your-Train/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Save-Your-Train
+//
+//  Created by Hugo Martin on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mobileApp/Save-Your-Train/Save_Your_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Save_Your_TrainApp.swift
+//  Save-Your-Train
+//
+//  Created by Hugo Martin on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Save_Your_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
