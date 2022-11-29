9102376ea0fa3a8836be2e661a868773963f6613
martihug <hugo.martin2@etu.univ-grenoble-alpes.fr>
2022-11-29 15:08:42 +0100

    add login page

---
+++ mobileApp/Save-Your-Train/Views/Account/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Save-Your-Train
+//
+//  Created by Hugo Martin on 29/11/2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
