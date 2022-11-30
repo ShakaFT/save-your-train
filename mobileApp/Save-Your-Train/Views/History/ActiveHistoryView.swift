61c74ac2a488c68f5961588f1f26a7bde3fe184a
martihug <hugo.martin2@etu.univ-grenoble-alpes.fr>
2022-11-30 17:48:33 +0100

    add remove history

--- mobileApp/Save-Your-Train/Views/History/ActiveHistoryView.swift
+++ mobileApp/Save-Your-Train/Views/History/ActiveHistoryView.swift
@@ -4,63 +4,71 @@ struct ActiveHistoryView: View {
     
     let history: HistoryModel
     
+    @State var deleteHistory: Bool = false
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                VStack(spacing: UIScreen.main.bounds.height * 0.05) {
-                    VStack {
-                        Text(Constants.getDate(timestamp: history.dateMs, format: .date)).font(.system(size: 30)).bold()
-                        Text(Constants.getDate(timestamp: history.dateMs, format: .hour)).font(.system(size: 30)).bold()
-                    }
-                    
-                    VStack (alignment: .leading ,spacing: UIScreen.main.bounds.height * 0.05) {
-                        if (!history.repetition.isEmpty){
-                            HStack {
-                                Text("Nombre de répetitions").font(.system(size: 20)).bold()
-                                Spacer()
-                                Text(history.repetition).font(.system(size: 20)).foregroundColor(.gray)
-                            }
-                        }
-                        
-                        if (!history.execution.isEmpty){
-                            HStack {
-                                Text("Temps d'exécution").font(.system(size: 20)).bold()
-                                Spacer()
-                                Text("\(history.execution) s").font(.system(size: 20)).foregroundColor(.gray)
-                            }
-                        }
-                        
-                        if (!history.weight.isEmpty){
-                            HStack {
-                                Text("Poids").font(.system(size: 20)).bold()
-                                Spacer()
-                                Text("\(history.weight) kg").font(.system(size: 20)).foregroundColor(.gray)
-                            }
-                        }
-                        
-                        if (!history.rest.isEmpty){
-                            HStack {
-                                Text("Temps de repos").font(.system(size: 20)).bold()
-                                Spacer()
-                                Text("\(history.rest) s").font(.system(size: 20)).foregroundColor(.gray)
-                            }
-                        }
-                        
-                        if (!history.series.isEmpty){
-                            HStack {
-                                Text("Nombre de séries").font(.system(size: 20)).bold()
-                                Spacer()
-                                Text(history.series).font(.system(size: 20)).foregroundColor(.gray)
-                            }
-                        }
-                    }
-                    .padding()
+        ZStack {
+            VStack(spacing: UIScreen.main.bounds.height * 0.05) {
+                VStack {
+                    Text(Constants.getDate(timestamp: history.dateMs, format: .date)).font(.system(size: 30)).bold()
+                    Text(Constants.getDate(timestamp: history.dateMs, format: .hour)).font(.system(size: 30)).bold()
                 }
                 
+                VStack (alignment: .leading ,spacing: UIScreen.main.bounds.height * 0.05) {
+                    if (!history.repetition.isEmpty){
+                        HStack {
+                            Text("Nombre de répetitions").font(.system(size: 20)).bold()
+                            Spacer()
+                            Text(history.repetition).font(.system(size: 20)).foregroundColor(.gray)
+                        }
+                    }
+                    
+                    if (!history.execution.isEmpty){
+                        HStack {
+                            Text("Temps d'exécution").font(.system(size: 20)).bold()
+                            Spacer()
+                            Text("\(history.execution) s").font(.system(size: 20)).foregroundColor(.gray)
+                        }
+                    }
+                    
+                    if (!history.weight.isEmpty){
+                        HStack {
+                            Text("Poids").font(.system(size: 20)).bold()
+                            Spacer()
+                            Text("\(history.weight) kg").font(.system(size: 20)).foregroundColor(.gray)
+                        }
+                    }
+                    
+                    if (!history.rest.isEmpty){
+                        HStack {
+                            Text("Temps de repos").font(.system(size: 20)).bold()
+                            Spacer()
+                            Text("\(history.rest) s").font(.system(size: 20)).foregroundColor(.gray)
+                        }
+                    }
+                    
+                    if (!history.series.isEmpty){
+                        HStack {
+                            Text("Nombre de séries").font(.system(size: 20)).bold()
+                            Spacer()
+                            Text(history.series).font(.system(size: 20)).foregroundColor(.gray)
+                        }
+                    }
+                }
+                .padding()
+                
+                Button(action: {self.deleteHistory.toggle()}){
+                    Image(systemName: "trash")
+                        .font(.largeTitle)
+                        .frame(width: 70, height: 70)
+                        .foregroundColor(.red)
+                }
             }
-            .navigationTitle(history.exerciseName)
-            .navigationBarTitleDisplayMode(.inline)
+            DeleteHistoryView(dateMs: self.history.dateMs, show: self.$deleteHistory)
+            
         }
+        .navigationTitle(history.exerciseName)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
